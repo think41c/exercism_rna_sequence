453faf45ebd2870f34fe482bc30097959c8cc5e7
Tony DiNitto <think41c@gmail.com>
2015-03-21 18:48:16 -0600

    Arranged methods to allow for arugement errors.

--- complement.rb
+++ complement.rb
@@ -1,11 +1,21 @@
 class Complement
   
   def self.of_dna(sequence)
+    sequence.chars do |x|
+      if x == "U"
+        raise ArgumentError, "Invalid DNA necleotide."
+      end
+    end
     sequence.tr("CGTA", "GCAU")
   end
 
   def self.of_rna(sequence)
-    sequence.tr("GCAU", "CGTA")
+    sequence.chars do |x|
+      if x == "T" 
+        raise ArguementError, "Invalid RNA nucleotide." 
+      end
+    end
+    sequence.tr("CGAU", "GCTA")
   end
 
 end
