23df31a1928a2446754cffbde2d6b2d4616e6bdd
Tony DiNitto <think41c@gmail.com>
2015-03-28 22:25:21 -0600

    Cleaned up method a bit.

--- complement.rb
+++ complement.rb
@@ -1,19 +1,13 @@
 class Complement
   
   def self.of_dna(sequence)
-
-    if sequence.chars.include?("U")
-      raise ArgumentError, "Invalid DNA nucleotide."
-    end
+    raise ArgumentError, "Invalid DNA nucleotide." if sequence.chars.include?("U")
     sequence.tr("CGTA", "GCAU")
   end
 
   def self.of_rna(sequence)
-    if sequence.chars.include?("T")
-      raise ArgumentError, "Invalid RNA nucleotide." 
-    end
+    raise ArgumentError, "Invalid RNA nucleotide." if sequence.chars.include?("T") 
     sequence.tr("CGAU", "GCTA")
   end
 
 end
-
